cb6e72cd69d5ad09679cf7f6226e268781949858
Lea Renmarker <lea.renmarker@solita.fi>
2021-09-14 16:14:27 +0200

    created new functions

--- projectscript1.py
+++ projectscript1.py
@@ -6,24 +6,23 @@ wind_id = 75
 consumption_id = 124
 variable_id = {'wind_id':75, 'consumption_id':124}
 personal_code = {'x-api-key': 'fXC7ucDVprBINXhgfBHh80Aq1SpIUPy9KahxZHfj'}
-#time_param = {'start_time': '2021-09-07T00:00:00Z', 'end_time': '2021-09-07T23:59:59Z'}
 
-
-#requests.get('https://api.fingrid.fi/v1/variable/{}/events/json'.format(variable_id), headers=header, params=time)
-
-#print(r.json())
-
-def get_time_param():
+def get_yesterday_date():
     yesterday = date.today() - datetime.timedelta(days=1)
+    return yesterday
+
+def create_time_param():
+    yesterday = get_yesterday_date()
     start = str(yesterday)+'T00:00:00Z'
     end = str(yesterday)+'T23:59:59Z'
     time_parameter = {'start_time': str(start),'end_time':str(end)}
     return time_parameter
 
-def create_file():
+
     #parameters
-    time_param = get_time_param()
-    yesterday = date.today() - datetime.timedelta(days=1)
+    '''time_param = create_time_param()
+    yesterday = get_yesterday_date()
+
 
     #create wind file and request wind data
     wind_file = open(str(yesterday)+'_wind.txt', 'w')
@@ -36,7 +35,23 @@ def create_file():
     consumption_file.write(json.dumps(consumption_request.json()))
 
     wind_file.close()
+    consumption_file.close()'''
+
+def create_wind_file():
+    wind_file = open(str(get_yesterday_date())+'_wind.txt', 'w')
+    wind_request = requests.get('https://api.fingrid.fi/v1/variable/{}/events/json'.format(wind_id), headers=personal_code, params=create_time_param())
+    wind_file.write(json.dumps(wind_request.json()))
+    wind_file.close()
+
+def create_consumption_file():
+    consumption_file = open(str(get_yesterday_date())+'_consumption.txt', 'w')
+    consumption_request = requests.get('https://api.fingrid.fi/v1/variable/{}/events/json'.format(consumption_id), headers=personal_code, params=create_time_param())
+    consumption_file.write(json.dumps(consumption_request.json()))
     consumption_file.close()
 
+def create_files():
+    create_wind_file()
+    create_consumption_file()
 
+create_files()
 
--- projectscript2.py
+++ projectscript2.py
@@ -4,18 +4,23 @@ import requests
 from datetime import date, timedelta
 import pandas as pd
 
-yesterday = date.today() - datetime.timedelta(days=1)
+def get_yesterday_date():
+    yesterday = date.today() - datetime.timedelta(days=1)
+    return yesterday
 
-wind_data = pd.read_json(str(yesterday)+'_wind.txt')
-total_wind = wind_data['value'].sum()
+def read_wind_data():
+    wind_data = pd.read_json(str(get_yesterday_date()) + '_wind.txt')
+    total_wind = wind_data['value'].sum()
+    return total_wind
 
-consump_data = pd.read_json(str(yesterday)+'_consumption.txt')
-total_consump = consump_data['value'].sum()
-print(total_wind)
-print(total_consump)
+def read_consump_data():
+    consump_data = pd.read_json(str(get_yesterday_date()) + '_consumption.txt')
+    total_consump = consump_data['value'].sum()
+    return total_consump
 
-daily_percent = total_wind/total_consump
-print(daily_percent)
+def store_coverage_data():
+    daily_percent = read_wind_data() / read_consump_data()
+    daily_stats = open('daily_stats.txt', 'a')
+    daily_stats.write('date: ' + str(get_yesterday_date()) + ' percentage: ' + str(daily_percent) + '\n')
 
-daily_stats = open('daily_stats.txt','a')
-daily_stats.write('date: '+str(yesterday)+ ' percentage: '+str(daily_percent)+'\n')
+store_coverage_data()
